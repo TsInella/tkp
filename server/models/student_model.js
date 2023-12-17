96e06fe35dcf2e3ddf5dddce959620b83ec44119
Hummmuss <90830731+Hummmuss@users.noreply.github.com>
2023-12-17 22:44:21 +0300

    Models fixed

--- server/models/student_model.js
+++ server/models/student_model.js
@@ -20,19 +20,11 @@ const Group = sequelize.define('group', {
     number: { type: DataTypes.STRING, primaryKey: true },
     tutorName: { type: DataTypes.STRING, allowNull: false },
     studentsNumber: { type: DataTypes.INTEGER },
-    // Внешний ключ для связи с факультетом
-    facultyId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'faculties',
-            key: 'id'
-        }
-    }
+
 });
 
 // Изменение модели студента для связи с курсами и группами
-const Students = sequelize.define('students', {
+const Student = sequelize.define('student', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     username: { type: DataTypes.STRING, unique: true, allowNull: false },
     surname: { type: DataTypes.STRING, allowNull: false },
@@ -43,39 +35,23 @@ const Students = sequelize.define('students', {
     fundingType: { type: DataTypes.STRING, allowNull: false },
     studyForm: { type: DataTypes.STRING, allowNull: false },
     educationLevel: { type: DataTypes.STRING, allowNull: false },
-    // Внешние ключи для связи со схемой
-    groupId: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        references: {
-            model: 'groups',
-            key: 'number'
-        }
-    },
-    courseId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'courses',
-            key: 'id'
-        }
-    }
-    // Необходимо удалить поля group и course, так как они теперь представлены как внешние ключи
+
 });
 
 // Связи между сущностями
-Faculty.hasMany(Group, { foreignKey: 'facultyId' });
-Group.belongsTo(Faculty, { foreignKey: 'facultyId' });
+Course.hasMany(Group)
+Group.belongsTo(Course)
 
-Group.hasMany(Students, { foreignKey: 'groupId' });
-Students.belongsTo(Group, { foreignKey: 'groupId' });
+Faculty.hasMany(Group)
+Group.belongsTo(Faculty)
 
-// Связь многие-ко-многим между курсами и группами
-// Также необходимо будет определить промежуточную таблицу для этой связи, если она уже не определена
+Group.hasOne(Student)
+Student.belongsTo(Group)
+
 
 module.exports = {
     Faculty,
     Course,
     Group,
-    Students
+    Student
 };
